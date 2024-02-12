aab9270f70f659d016159ad32ae6ce6e4039fdf1
YULIA VASILENKO <yuliav@devart.com>
2024-02-12 16:08:30 +0200

    initial commit

---
+++ Tables/dbo.Table1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table1] (
+  [id] [int] NOT NULL,
+  [name] [nvarchar](255) NULL,
+  [age] [int] NULL,
+  PRIMARY KEY CLUSTERED ([id])
+)
+ON [PRIMARY]
+GO
---
+++ Tables/dbo.Table2.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table2] (
+  [id] [int] NOT NULL,
+  [title] [nvarchar](255) NULL,
+  [description] [text] NULL,
+  [created_at] [datetime] NULL,
+  PRIMARY KEY CLUSTERED ([id])
+)
+ON [PRIMARY]
+TEXTIMAGE_ON [PRIMARY]
+GO
