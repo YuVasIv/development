b581ab1c51b717d451e55e389e7b3707a400fb78
jordansanders <jordansanders@devart.com>
2024-05-24 16:32:23 +0300

    initial commit

---
+++ Tables/dbo.customers.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[customers] (
+  [customer_id] [int] NOT NULL,
+  [customer_name] [varchar](100) NULL,
+  [email] [varchar](100) NULL,
+  [phone] [varchar](20) NULL,
+  [address] [varchar](255) NULL,
+  PRIMARY KEY CLUSTERED ([customer_id])
+)
+ON [PRIMARY]
+GO
---
+++ Tables/dbo.products.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[products] (
+  [product_id] [int] NOT NULL,
+  [product_name] [varchar](100) NULL,
+  [price] [decimal](10, 2) NULL,
+  [stock_quantity] [int] NULL,
+  PRIMARY KEY CLUSTERED ([product_id])
+)
+ON [PRIMARY]
+GO
