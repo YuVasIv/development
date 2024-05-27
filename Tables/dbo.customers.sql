0f580b0505e7ec8d577906eea386f2e50f5af12f
jordansanders <jordansanders@devart.com>
2024-05-27 14:20:03 +0300

    commit the changes

--- Tables/dbo.customers.sql
+++ Tables/dbo.customers.sql
@@ -1,10 +1,10 @@
 ﻿CREATE TABLE [dbo].[customers] (
   [customer_id] [int] NOT NULL,
-  [customer_name] [varchar](85) NULL,
+  [customer_name] [varchar](100) NULL,
   [email] [varchar](100) NULL,
   [phone] [varchar](20) NULL,
   [address] [varchar](255) NULL,
-  [city] [varchar](50) NULL,
+  [city] [varchar](85) NULL,
   PRIMARY KEY CLUSTERED ([customer_id])
 )
 ON [PRIMARY]
