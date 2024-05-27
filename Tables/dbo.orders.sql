95bd1706bd749497dd6f6066c091203a9432322b
jordansanders <jordansanders@devart.com>
2024-05-27 15:10:12 +0300

    n

---
+++ Tables/dbo.orders.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[orders] (
+  [order_id] [int] NOT NULL,
+  [customer_id] [int] NULL,
+  [order_date] [date] NULL,
+  [order_amount] [decimal](10, 2) NULL,
+  [order_status] [varchar](50) NULL,
+  [shipping_address] [varchar](255) NULL,
+  PRIMARY KEY CLUSTERED ([order_id])
+)
+ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[orders] WITH NOCHECK
+  ADD FOREIGN KEY ([customer_id]) REFERENCES [dbo].[customers] ([customer_id])
+GO
