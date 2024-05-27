ac580b81bbbe239fb1aab719c706e46a15d7d787
jordansanders <jordansanders@devart.com>
2024-05-27 13:24:50 +0300

    1

--- Tables/dbo.customers.sql
+++ Tables/dbo.customers.sql
@@ -1,9 +1,10 @@
 ﻿CREATE TABLE [dbo].[customers] (
   [customer_id] [int] NOT NULL,
-  [customer_name] [varchar](100) NULL,
+  [customer_name] [varchar](85) NULL,
   [email] [varchar](100) NULL,
   [phone] [varchar](20) NULL,
   [address] [varchar](255) NULL,
+  [city] [varchar](50) NULL,
   PRIMARY KEY CLUSTERED ([customer_id])
 )
 ON [PRIMARY]
